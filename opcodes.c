77b559e36b02b6aec9fe6f6c7396c9651fbabae7
Annabelle Platt <aplatt@olin.edu>
2022-03-03 11:34:59 -0500

    Add opcodes 1nnn and 2nnn

--- opcodes.c
+++ opcodes.c
@@ -13,4 +13,20 @@ void op_00EE() {
     PC = chip.stack[chip8.stack_pointer];
 }
 
+// Jump to location nnn in memory
+void op_1nnn() {
+    // Mask to grab only the last three bytes of the opcode
+    // then set the program counter to that address 
+    PC = opcode & 0x0FFF;
+}
+
+// Call subroutine at nnn
+void op_2nnn() {
+    //store where we are
+    stack[stack_pointer] = pc;
+    // now increment the stack pointer 
+    ++stack_pointer;
+    // and now jump to the new address
+    PC = opcode & 0x0FFF;
+}
 
